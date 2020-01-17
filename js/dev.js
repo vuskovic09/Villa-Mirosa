7e996567be39225497dd8659d2ea3faccc27745b
Filip Vušković <filip.vuskovic09@gmail.com>
2020-01-17 13:00:20 +0100

    MOD: js cleanup

--- js/dev.js
+++ js/dev.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
     $("#tab--1").click(function(){
-        $('.second-section').css({
-            'display': 'none'
-        });
-        $('.first-section').fadeToggle(1500);
+        $('.second-section').fadeOut(1500);
+        $('.first-section').fadeIn(1500);
     });
     $("#tab--2").click(function(){
-        $('.first-section').css({
-            'display': 'none'
-        });
-        $('.second-section').fadeToggle(1500);
+        $('.first-section').fadeOut(1500);
+        $('.second-section').fadeIn(1500);
     });
 });
